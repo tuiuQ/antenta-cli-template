afe7dd3b180b929f7d89fcf67c5dda48b3fcab05
tuiu <13719283454@163.com>
2021-07-13 03:39:01 +0800

    webpack模版基本架子，切换其他分支在进行测试

--- src/components/switch/index.ts
+++
@@ -1,8 +0,0 @@
-import Switch from './src'
-import type { App } from 'vue'
-
-Switch.install = (app: App): void => {
-  app.component(Switch.name, Switch)
-}
-
-export default Switch
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
 import App from './demo'
-import GlobalComponents from './lib/globalComponents'
 
 const app = createApp(App)
 
-app.use(GlobalComponents)
 app.mount("#app")
