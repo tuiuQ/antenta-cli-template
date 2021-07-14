47eaa195c54c49d55de3ff791be8ab24ddf6250c
tuiu <13719283454@163.com>
2021-07-14 15:25:16 +0800

    日常提交

---
+++ build/webpack.config.ts
@@ -0,0 +1,9 @@
+import { Configuration } from 'webpack'
+import merge from 'webpack-merge'
+import base from './webpack.config.base'
+import dev from './webpack.config.dev'
+import prod from './webpack.config.prod'
+
+export default {
+  
+} as Configuration
